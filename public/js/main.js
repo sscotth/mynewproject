669cd782ff13d9bafae808a749887828a3391d09
Scott <scott@sscotth.io>
2015-02-04 01:21:00 -0600

    reconfigure

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+;(function () {
+  'use strict';
+
+  var hello = 'world';
+
+  function omg() {
+    console.log('test');
+    return true;
+  }
+
+}());
+
+
+function hello() {
+  'use strict';
+  return 'world';
+}
+
+hello();
---
+++ test/main.test.js
@@ -0,0 +1,22 @@
+/* jshint mocha: true, expr: true */
+
+;(function () {
+  'use strict';
+
+  describe('test suite', function () {
+    describe('mocha and chai', function () {
+      it('should assert true', function () {
+        true.should.be.true;
+        false.should.be.false; // false.should.be.true should fail by default
+      });
+    });
+  });
+
+  describe('hello', function () {
+    it('should return "world"', function () {
+      /* global hello */
+      hello().should.equal('world');
+    });
+  });
+
+}());
